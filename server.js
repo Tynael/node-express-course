10934a410e1329e76712ecea49d5f7b8a81af736
DESKTOP-GH782NV\Carol <pelucaroltheo@gmail.com>
2022-03-14 20:38:13 +0200

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+
 
 const mockUserData = [
     {name: 'Mark'},
@@ -20,6 +23,27 @@ app.get('/users/:id', function(req, res) {
         message: 'Got one user!',
         user: req.params.id
     });
+});
+
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'Password and username match!',
+            token: 'Encrypted token goes here.'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'Password and username do not match!'
+        });
+    }
 });
 
 app.listen(8000, () => {
